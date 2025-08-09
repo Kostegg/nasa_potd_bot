fa71297398fec2b417b3c79da2087dfed0c69615
Kostegg <kostik20001@gmail.com>
2025-08-10 02:48:35 +0300

    Now one method is used for both specific and current date

--- api/nasa.py
+++ api/nasa.py
@@ -3,10 +3,8 @@ from config import nasa_api_token
 
 url = f"https://api.nasa.gov/planetary/apod?api_key={nasa_api_token}"
 
-def cur_day_apod():
-    return requests.get(url).json()
-
-
-def specific_day_apod(date: str):
-    new_url = url + "&date=" + date
-    return requests.get(new_url).json()
+def apod(date: None | str):
+    if date:
+        return requests.get(url+"&date="+date).json()
+    else:
+        return requests.get(url).json()
