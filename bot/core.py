f2dbacc35b12235b3ba9f868eb2d03bdc2511851
Kostegg <kostik20001@gmail.com>
2025-08-10 03:01:37 +0300

    Finalised pic functionality and added help function

--- bot/core.py
+++ bot/core.py
@@ -1,7 +1,11 @@
 import logging
-from config import api_token
+from operator import contains
+
+from config import tg_api_token
 from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler, MessageHandler
+from api.nasa import apod
+import re
 
 logging.basicConfig(
     format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
@@ -10,15 +14,43 @@ logging.basicConfig(
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="Hi! This bot doesn't contain alot right now.\n"
+                                                                          "use /pic to get a picture or /help to get more info!")
 
+async def pic(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if context.args:
+        info = apod(context.args[0])
+        if info.get("code") is not None or len(context.args) != 1:
+            await (context.bot.send_message(chat_id=update.effective_chat.id, text="Please input correct date in YYYY-MM-DD format!"))
+            return
+    else:
+        info = apod(None)
+    await (context.bot.send_photo(chat_id=update.effective_chat.id, photo=info["url"]))
+    await (context.bot.send_message(chat_id=update.effective_chat.id, text=info["explanation"]+"\n"
+                                                                           +"Copyright: "
+                                                                           +(info["copyright"] if "copyright" in info else "")+"\n"+
+                                    "Date of APOD: "+info["date"]))
 
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await (context.bot.send_message(chat_id=update.effective_chat.id, text="Here are the commands that are usable with this bot:\n"
+                                                                           "/start - Start the bot and get a small message\n"
+                                                                           "/help - Displays this message\n"
+                                                                           "/pic [date in YYYY-MM-DD format or nothing] - sends a NASA APOD of specific date or of the current day"))
+
+async def unknown(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await (context.bot.send_message(chat_id=update.effective_chat.id, text="Sorry, I don't support any interactions with me beside '/start', '/help' and '/pic' commands at the moment"))
 
 
 if __name__ == '__main__':
-    application = ApplicationBuilder().token(f'{api_token}').build()
+    application = ApplicationBuilder().token(f'{tg_api_token}').build()
 
     start_handler = CommandHandler('start', start)
+    pic_handler = CommandHandler('pic', pic)
+    help_handler = CommandHandler('help', help)
+    unknown_handler = MessageHandler(None, unknown)
     application.add_handler(start_handler)
+    application.add_handler(pic_handler)
+    application.add_handler(help_handler)
+    application.add_handler(unknown_handler)
 
     application.run_polling()
